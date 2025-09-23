e0a3361b4f33de0a2b0a548eaa9ee0a1455dea8e
Chong Zheng Hong <zhchong@cmg.com.my>
2025-09-23 11:10:32 +0800

    -fixed vertical tab bar view alignment

--- packages/jc_widgets/lib/src/vertical_tab_bar_view.dart
+++ packages/jc_widgets/lib/src/vertical_tab_bar_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
-class VerticalTabBarView extends StatefulWidget{
+class VerticalTabBarView extends StatefulWidget {
   final int itemCount;
-  final Widget Function(BuildContext context, int index, bool currentTab)? tabBarBuilder;
+  final Widget Function(BuildContext context, int index, bool currentTab)?
+      tabBarBuilder;
   final Widget Function(BuildContext context, int index)? tabBarViewBuilder;
   final Widget Function(BuildContext context, int index)? indicatorBuilder;
   final void Function(int index)? onPageChanged;
@@ -35,8 +36,8 @@ class VerticalTabBarView extends StatefulWidget{
   State<VerticalTabBarView> createState() => _VerticalTabBarViewState();
 }
 
-class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProviderStateMixin{
-
+class _VerticalTabBarViewState extends State<VerticalTabBarView>
+    with TickerProviderStateMixin {
   late TabController _tabController;
   final PageController _pageController = PageController();
 
@@ -50,8 +51,12 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
   final GlobalKey _stackKey = GlobalKey();
 
   Duration get tabAnimateDuration => Duration(
-    microseconds: widget.tabAnimatedDuration.inMicroseconds + (widget.tabAnimatedDuration.inMicroseconds/10 * (currentIndex - previousIndex).abs()).toInt(),
-  );
+        microseconds: widget.tabAnimatedDuration.inMicroseconds +
+            (widget.tabAnimatedDuration.inMicroseconds /
+                    10 *
+                    (currentIndex - previousIndex).abs())
+                .toInt(),
+      );
 
   @override
   void initState() {
@@ -63,7 +68,7 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
   void didUpdateWidget(covariant VerticalTabBarView oldWidget) {
     super.didUpdateWidget(oldWidget);
 
-    if(oldWidget.itemCount != widget.itemCount){
+    if (oldWidget.itemCount != widget.itemCount) {
       setState(() {
         _tabController.dispose();
         _setupTabController();
@@ -71,13 +76,15 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
     }
   }
 
-  void _setupTabController(){
+  void _setupTabController() {
     _tabController = TabController(length: widget.itemCount, vsync: this);
     globalList = List.generate(widget.itemCount, (e) => GlobalKey());
     currentIndex = 0;
 
-    SchedulerBinding.instance.addPostFrameCallback((timeStamp){
-      currentTabContext.value = globalList.isNotEmpty ? globalList[currentIndex].currentContext : null;
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      currentTabContext.value = globalList.isNotEmpty
+          ? globalList[currentIndex].currentContext
+          : null;
     });
   }
 
@@ -86,66 +93,70 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
     return _buildSecondApproach(context);
   }
 
-  Widget _buildSecondApproach(BuildContext context){
+  Widget _buildSecondApproach(BuildContext context) {
     int flex = (100 * widget.flex).toInt();
 
     return Row(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Expanded(
-          flex: flex,
-          child: Container(
-            constraints: BoxConstraints(
-              maxWidth: widget.tabBarMaxWidth ?? double.infinity
-            ),
-            decoration: widget.tabBarDecoration,
-            child: SingleChildScrollView(
-              child: Stack(
-                key: _stackKey,
-                children: [
-                  ValueListenableBuilder(
-                    valueListenable: currentTabContext,
-                    builder: (BuildContext context, BuildContext? value, Widget? child) {
-                      if(value?.findRenderObject() != null && _stackKey.currentContext?.findRenderObject() != null){
-                        final box = value!.findRenderObject() as RenderBox;
-                        final stackBox = _stackKey.currentContext!.findRenderObject() as RenderBox;
-                        final offset = box.localToGlobal(Offset.zero, ancestor: stackBox);
-                        return AnimatedPositioned(
-                          top: offset.dy,
-                          left: 0,
-                          height: box.size.height,
-                          width: box.size.width,
-                          duration: tabAnimateDuration,
-                          child: _buildIndicator(context, currentIndex)
-                        );
-                      }
+            flex: flex,
+            child: Container(
+              constraints: BoxConstraints(
+                  maxWidth: widget.tabBarMaxWidth ?? double.infinity),
+              decoration: widget.tabBarDecoration,
+              child: SingleChildScrollView(
+                child: Stack(
+                  key: _stackKey,
+                  children: [
+                    ValueListenableBuilder(
+                      valueListenable: currentTabContext,
+                      builder: (BuildContext context, BuildContext? value,
+                          Widget? child) {
+                        if (value?.findRenderObject() != null &&
+                            _stackKey.currentContext?.findRenderObject() !=
+                                null) {
+                          final box = value!.findRenderObject() as RenderBox;
+                          final stackBox = _stackKey.currentContext!
+                              .findRenderObject() as RenderBox;
+                          final offset = box.localToGlobal(Offset.zero,
+                              ancestor: stackBox);
+                          return AnimatedPositioned(
+                              top: offset.dy,
+                              left: 0,
+                              height: box.size.height,
+                              width: box.size.width,
+                              duration: tabAnimateDuration,
+                              child: _buildIndicator(context, currentIndex));
+                        }
 
-                      return const SizedBox();
-                    },
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: List.generate(
-                      widget.itemCount,
-                          (index){
-                        Widget tab = widget.tabBarBuilder?.call(context, index, index == currentIndex) ?? Tab(text: 'Tab $index',);
+                        return const SizedBox();
+                      },
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: List.generate(widget.itemCount, (index) {
+                        Widget tab = widget.tabBarBuilder
+                                ?.call(context, index, index == currentIndex) ??
+                            Tab(
+                              text: 'Tab $index',
+                            );
 
                         return _buildTab(context, tab, index);
-                      }
+                      }),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        ),
+            )),
         Expanded(
           flex: 100 - flex,
           child: PageView.builder(
             controller: _pageController,
             itemCount: widget.itemCount,
             itemBuilder: (context, index) {
-              if(widget.tabBarViewBuilder != null) return widget.tabBarViewBuilder!(context, index);
+              if (widget.tabBarViewBuilder != null)
+                return widget.tabBarViewBuilder!(context, index);
 
               return Center(
                 child: Text(
@@ -153,12 +164,12 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
                 ),
               );
             },
-            onPageChanged: (index){
-              if(!_changeViewByTap) {
+            onPageChanged: (index) {
+              if (!_changeViewByTap) {
                 _setIndex(index);
                 _scrollToItem(globalList[index]);
               }
-              if(currentIndex == index) _changeViewByTap = false;
+              if (currentIndex == index) _changeViewByTap = false;
 
               setState(() {});
             },
@@ -168,30 +179,30 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
     );
   }
 
-  Widget _buildTab(BuildContext context, Widget tab, int index){
+  Widget _buildTab(BuildContext context, Widget tab, int index) {
     return Stack(
       key: globalList[index],
       children: [
         /*Positioned.fill(
           child: currentIndex == index ? _buildIndicator(context, index) : const SizedBox(),
         ),*/
-        Align(
-          alignment: Alignment.center,
+        SizedBox(
+          width: double.infinity,
           child: _buildTabWidget(tab, currentIndex == index),
         ),
         Positioned.fill(
           child: InkWell(
-            onTap: (){
+            onTap: () {
               setState(() {
                 _changeViewByTap = true;
                 _setIndex(index);
               });
               _scrollToItem(globalList[index]);
-              widget.animateToPage ? _pageController.animateToPage(
-                  index,
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut
-              ) : _pageController.jumpToPage(index);
+              widget.animateToPage
+                  ? _pageController.animateToPage(index,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut)
+                  : _pageController.jumpToPage(index);
             },
           ),
         )
@@ -199,27 +210,28 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
     );
   }
 
-  Widget _buildTabWidget(Widget tab, bool currentTab){
-    if(tab is! Tab) return tab;
+  Widget _buildTabWidget(Widget tab, bool currentTab) {
+    if (tab is! Tab) return tab;
 
-    if(tab.child != null) return tab.child!;
+    if (tab.child != null) return tab.child!;
 
     return Container(
       padding: const EdgeInsets.all(10),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          if(tab.icon != null)
+          if (tab.icon != null)
             Container(
               margin: tab.iconMargin,
               child: tab.icon,
             ),
-          if(tab.text != null)
+          if (tab.text != null)
             Flexible(
               child: Text(
                 tab.text!,
                 softWrap: true,
-                style: currentTab ? widget.textStyle : widget.unselectedTextStyle,
+                style:
+                    currentTab ? widget.textStyle : widget.unselectedTextStyle,
               ),
             )
         ],
@@ -227,15 +239,16 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
     );
   }
 
-  Widget _buildIndicator(BuildContext context, int index){
-    if(widget.indicatorBuilder != null) return widget.indicatorBuilder!(context, index);
+  Widget _buildIndicator(BuildContext context, int index) {
+    if (widget.indicatorBuilder != null)
+      return widget.indicatorBuilder!(context, index);
 
     return Container(
       color: Colors.white,
     );
   }
 
-  void _setIndex(int index){
+  void _setIndex(int index) {
     previousIndex = currentIndex;
     currentIndex = index;
 
@@ -243,12 +256,9 @@ class _VerticalTabBarViewState extends State<VerticalTabBarView> with TickerProv
   }
 
   void _scrollToItem(GlobalKey key) async {
-    if(key.currentContext != null){
-      Scrollable.ensureVisible(
-        key.currentContext!,
-        alignment: .5,
-        duration: const Duration(milliseconds: 300)
-      );
+    if (key.currentContext != null) {
+      Scrollable.ensureVisible(key.currentContext!,
+          alignment: .5, duration: const Duration(milliseconds: 300));
 
       currentTabContext.value = key.currentContext;
     }
